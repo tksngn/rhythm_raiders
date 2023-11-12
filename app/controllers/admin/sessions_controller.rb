2f81515b77782bec2c33aa074ca5d25604b326f1
tksngn <138876045+tksngn@users.noreply.github.com>
2023-11-12 08:27:29 +0000

    [fix]会員一覧着手

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,18 +1,6 @@
 # frozen_string_literal: true
 
 class Admin::SessionsController < Devise::SessionsController
-
-  def new
-
-  end
-
-  def create
-    self.resource = warden.authenticate!(auth_options)
-    set_flash_message!(:notice, :signed_in)
-    sign_in(resource_name, resource)
-    yield resource if block_given?
-    respond_with resource, location: after_sign_in_path_for(resource)
-  end
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
