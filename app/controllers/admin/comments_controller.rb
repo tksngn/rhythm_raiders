d53c071e98fd77044874f1c4e59edbe26a29f7fe
tksngn <138876045+tksngn@users.noreply.github.com>
2023-12-20 20:36:36 +0000

    デプロイ1-1

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -1,6 +1,6 @@
 class Admin::CommentsController < ApplicationController
   def destroy
-    comment = PostComment.find(params[:id])
+    comment = Comment.find(params[:id])
     comment.destroy
     redirect_to admin_member_path(comment.member_id)
   end
