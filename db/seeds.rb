8b2018da1836c7139145c48331a367b3c83d751a
tksngn <138876045+tksngn@users.noreply.github.com>
2023-12-22 07:25:15 +0000

    [fix]デプロイ1-3

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,6 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Admin.create!(
-  email: 'kashy@raiders.com',
-  password: 'rhythmkaiser'
+  email: 'raiders@masters.com',
+  password: 'rhythmmaster'
 )
