fa058387b00793ecbde2468169220ee89d58cb39
tksngn <138876045+tksngn@users.noreply.github.com>
2023-12-25 11:19:30 +0000

    [Fix] seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -41,21 +41,21 @@ end
 
 CreatedTrack.find_or_create_by!(music_title: "Fantasy World") do |created_track|
   created_track.music_genre = "Orchestra"
-  created_track.music_file = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/fantasy-background-music-110593.mp3"), filename:"fantasy-background-music-110593.mp3")
+  created_track.music_file = File.open("#{Rails.root}/db/fixtures/fantasy-background-music-110593.mp3")
   created_track.creater_word = "壮大な感じの素晴らしい曲に仕上がりました！"
   created_track.member = Test01
 end
 
 CreatedTrack.find_or_create_by!(music_title: "Medieval Cityscape") do |created_track|
   created_track.music_genre = "Medieval BGM"
-  created_track.music_file = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/field.mp3"), filename:"field.mp3")
+  created_track.music_file = File.open("#{Rails.root}/db/fixtures/field.mp3")
   created_track.creater_word = "中世の街並みを表現できるいい曲になりました！"
   created_track.member = Test02
 end
 
 CreatedTrack.find_or_create_by!(music_title: "Turmoil of Battle") do |created_track|
   created_track.music_genre = "Battle BGM"
-  created_track.music_file = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/worldsend.mp3"), filename:"worldsend.mp3")
+  created_track.music_file = File.open("#{Rails.root}/db/fixtures/worldsend.mp3")
   created_track.creater_word = "激しい戦いの火蓋が切って落とされました！"
   created_track.member = Test03
 end
